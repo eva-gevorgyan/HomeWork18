24b22e2a1e58cac6f6c0f633c9c3c086d86afbe0
eva-gevorgyan <127292682+eva-gevorgyan@users.noreply.github.com>
2023-04-22 10:36:01 +0400

    Add files via upload

---
+++ DBFirstExample/Controllers/VegaBrandController.cs
@@ -0,0 +1,50 @@
+﻿using Azure;
+using DBFirstExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace DBFirstExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VegaBrandController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        public VegaBrandController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public string GetVegaBrand()
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("VegaVestaNewCon").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM VegaBrand", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            List<VegaBrand> vegaVestaList = new List<VegaBrand>();
+            //Response response = new Response();
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    VegaBrand vegaVesta = new VegaBrand();
+                    vegaVesta.Id = Convert.ToInt32(dt.Rows[i]["ID"]);
+                    vegaVesta.VegaBrandName = Convert.ToString(dt.Rows[i]["VegaBrandName"]);
+                    vegaVestaList.Add(vegaVesta);
+                }
+            }
+            //if (vegaVestaList.Count > 0)
+            //    return JsonConvert.SerializeObject(vegaVestaList);
+            //else
+            //{
+            //    response.StatusCode = 100;
+            //    response.ErrorMessage = "Not found";
+            //    return JsonConvert.SerializeObject(response);
+            //}
+            return JsonConvert.SerializeObject(vegaVestaList);
+        }
+    }
+}
---
+++ DBFirstExample/Controllers/VegaCategoryController.cs
@@ -0,0 +1,44 @@
+﻿using DBFirstExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace DBFirstExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VegaCategoryController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        public VegaCategoryController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public string GetVegaCategory()
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("VegaVestaNewCon").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM VegaCategory", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            List<VegaCategory> vegaVestaList = new List<VegaCategory>();
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    VegaCategory vegaCategory = new VegaCategory();
+                    vegaCategory.Id = Convert.ToInt32(dt.Rows[i]["ID"]);
+                    vegaCategory.VegaObject = Convert.ToString(dt.Rows[i]["VegaObject"]);
+                    vegaCategory.VegaKitchen = Convert.ToBoolean(dt.Rows[i]["VegaKitchen"]);
+                    vegaCategory.VegaDevice = Convert.ToBoolean(dt.Rows[i]["VegaDevice"]);
+                    vegaCategory.VegaFurniture = Convert.ToBoolean(dt.Rows[i]["VegaFurniture"]);
+                    vegaVestaList.Add(vegaCategory);
+                }
+            }
+            return JsonConvert.SerializeObject(vegaVestaList);
+        }
+    }
+}
